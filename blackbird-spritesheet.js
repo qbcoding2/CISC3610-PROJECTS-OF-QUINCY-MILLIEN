50c0bca23ed07a1b539c88c96c7543065f2c30d8
quincy <millienq2@gmail.com>
2022-10-10 14:22:34 -0400

    Update blackbird-spritesheet.js

--- blackbird-spritesheet.js
+++ blackbird-spritesheet.js
@@ -1,187 +1,82 @@
 blackbird = {
-	src: "blackbird-spritesheet.png",
+	src: "black-birdsplitsheet.png",
 	img: undefined,
 	frame: 0,
 	offset: 0,
-	"frames": [
-        {
-		"filename" : "01.png", 
-			"frame": {
-				"x": 1,
-				"y": 1,
-				"w": 1000,
-				"h": 1000
+		"frames": [
+			{
+				"filename": "01.gif",
+				"frame": {
+					"x": 1,
+					"y": 1,
+					"w": 480,
+					"h": 464
+				},
+				"rotated": false,
+				"trimmed": false,
+				"spriteSourceSize": {
+					"x": 0,
+					"y": 0,
+					"w": 480,
+					"h": 464
+				},
+				"sourceSize": {
+					"w": 480,
+					"h": 464
+				}
 			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 1000,
-				"h": 1000
+			{
+				"filename": "02.gif",
+				"frame": {
+					"x": 483,
+					"y": 1,
+					"w": 480,
+					"h": 464
+				},
+				"rotated": false,
+				"trimmed": false,
+				"spriteSourceSize": {
+					"x": 0,
+					"y": 0,
+					"w": 480,
+					"h": 464
+				},
+				"sourceSize": {
+					"w": 480,
+					"h": 464
+				}
 			},
-			"sourceSize": {
-				"w": 1000,
-				"h": 1000
+			{
+				"filename": "03.gif",
+				"frame": {
+					"x": 1,
+					"y": 467,
+					"w": 480,
+					"h": 464
+				},
+				"rotated": false,
+				"trimmed": false,
+				"spriteSourceSize": {
+					"x": 0,
+					"y": 0,
+					"w": 480,
+					"h": 464
+				},
+				"sourceSize": {
+					"w": 480,
+					"h": 464
+				}
 			}
-		},
-        {
-		"filename" : "02.png", 
-			"frame": {
-				"x": 1003,
-				"y": 1,
-				"w": 1000,
-				"h": 1000
+		],
+		"meta": {
+			"app": "http://www.codeandweb.com/texturepacker",
+			"version": "1.0",
+			"image": "black-birdsplitsheet.png",
+			"format": "RGBA8888",
+			"size": {
+				"w": 964,
+				"h": 932
 			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 1000,
-				"h": 1000
-			},
-			"sourceSize": {
-				"w": 1000,
-				"h": 1000
-			}
-		},
-        {
-		"filename" : "03.png", 
-			"frame": {
-				"x": 1,
-				"y": 1003,
-				"w": 1000,
-				"h": 1000
-			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 1000,
-				"h": 1000
-			},
-			"sourceSize": {
-				"w": 1000,
-				"h": 1000
-			}
-		},
-        {
-		"filename" : "04.png",
-			"frame": {
-				"x": 1003,
-				"y": 1003,
-				"w": 1000,
-				"h": 1000
-			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 1000,
-				"h": 1000
-			},
-			"sourceSize": {
-				"w": 1000,
-				"h": 1000
-			}
-		},
-        {
-		"filename" : "05.png",
-			"frame": {
-				"x": 2005,
-				"y": 1,
-				"w": 1000,
-				"h": 1000
-			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 1000,
-				"h": 1000
-			},
-			"sourceSize": {
-				"w": 1000,
-				"h": 1000
-			}
-		},
-        {
-		"filename" : "06.png",
-			"frame": {
-				"x": 2005,
-				"y": 1003,
-				"w": 1000,
-				"h": 1000
-			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 1000,
-				"h": 1000
-			},
-			"sourceSize": {
-				"w": 1000,
-				"h": 1000
-			}
-		},
-        {
-		"filename" : "07.png",
-			"frame": {
-				"x": 1,
-				"y": 2005,
-				"w": 1000,
-				"h": 1000
-			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 1000,
-				"h": 1000
-			},
-			"sourceSize": {
-				"w": 1000,
-				"h": 1000
-			}
-		},
-        {
-		"filename" : "08.png",
-			"frame": {
-				"x": 1003,
-				"y": 2005,
-				"w": 1000,
-				"h": 1000
-			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 1000,
-				"h": 1000
-			},
-			"sourceSize": {
-				"w": 1000,
-				"h": 1000
-			}
-	    }
-    ],
-	"meta": {
-		"app": "http://www.codeandweb.com/texturepacker",
-		"version": "1.0",
-		"image": "blackbird-spritesheet.png",
-		"format": "RGBA8888",
-		"size": {
-			"w": 3006,
-			"h": 3006
-		},
-		"scale": "1"
+			"scale": "1"
+		}
 	}
-}
